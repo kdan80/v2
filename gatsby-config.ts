ba4f07aa1686efc8c6a003f070e54b8d7bb70bed
kdan80 <kierandansey80@gmail.com>
2022-06-08 18:28:19 +0100

    Updated gatsby config & added content

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,34 +1,53 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `kierandansey.co.uk`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+    siteMetadata: {
+        title: "Kieran Dansey",
+        description: "Kieran Dansey freelance web developer",
+        siteUrl: "https://kierandansey.co.uk", // No trailing slash allowed!
+        image: "/og.webp" // Path to your image you placed in the 'static' folder
+    },
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        "gatsby-plugin-styled-components", 
+        "gatsby-plugin-image", 
+        "gatsby-plugin-react-helmet", {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        }, 
+        "gatsby-plugin-mdx", 
+        "gatsby-plugin-sharp", 
+        "gatsby-transformer-sharp", 
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": `${__dirname}/src/images/`
+            },
+            __key: "images"
+      }, 
+      {
+          resolve: 'gatsby-source-filesystem',
+          options: {
+              "name": "pages",
+              "path": `${__dirname}/pages/`
+          },
+          __key: "pages"
+      },
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: "content",
+          path: `${__dirname}/content/`,
+        },
+        __key: "content"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    ]
 };
 
 export default config;
