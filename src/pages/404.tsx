0608a096c17abe12c5aad28e86405aa1596fac89
kdan80 <kierandansey80@gmail.com>
2022-06-08 18:07:40 +0100

    Gatsby setup

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+
+
+// markup
+const NotFoundPage = () => {
+  return (
+    <main>
+      404 Not Found
+    </main>
+  )
+}
+
+export default NotFoundPage
