8b70a2d0e07bdb9c02cbe7271ff3b2936967ac11
kdan80 <kierandansey80@gmail.com>
2022-06-08 18:15:58 +0100

    Added fonts, icons, static & config

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+module.exports = {
+    email: "kierandansey80@gmail.com",
+
+    skills: [
+        "HTML", 
+        "JavaScript (ES6+)", 
+        "CSS/SASS", 
+        "React", 
+        "Gatsby", 
+        "Node.js/Express", 
+        "Python/Django", 
+        "Linux/Bash", 
+        "MongoDB", 
+        "GraphQL", 
+        "Git", 
+        "SVG/Inkscape"
+    ],
+
+    navLinks: [
+        {
+            name: "About",
+            url:"/#about"
+        },
+        {
+            name: "My Projects",
+            url:"/#projects"
+        },
+        {
+            name: "Contact Me",
+            url:"/#contact"
+        }
+    ]
+}
