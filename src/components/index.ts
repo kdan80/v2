8b4d847b025ddb00920f97400eb61d783a31f80e
kdan80 <kierandansey80@gmail.com>
2022-06-10 19:24:53 +0100

    Added Footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export { default as DocumentHead }      from "./DocumentHead"
+export { default as DocumentHead }      from "./DocumentHead";
+export { default as Footer }            from "./Footer";
 export { default as Layout }            from "./Layout";
