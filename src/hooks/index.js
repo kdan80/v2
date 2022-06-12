1c10e48c5f08d8490c42b10486b2e61ddb061fad
kdan80 <kierandansey80@gmail.com>
2022-06-12 16:34:46 +0100

    Removed compiled ts files that were accidently added by tsc --watch

--- src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useScrollDirection } from "./useScrollDirection";
-export { default as useScrolledToTop } from "./useScrolledToTop";
-export { default as useViewportHeight } from "./useViewportHeight";
